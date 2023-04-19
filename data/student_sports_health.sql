a6fd2c8fdabb9cc1c8bed8d38022c76356525f83
dornsmin <130754080+dornsmin@users.noreply.github.com>
2023-04-19 17:52:09 +0800

    Add files via upload

---
+++ data/student_sports_health.sql
@@ -0,0 +1,61 @@
+--DB:bdlab2013
+
+--查找所有指标：
+SELECT
+ t2.ST_CLASS_ID,
+ t2.ST_CLASS_GRADE_CODE,
+ t2.ST_CLASS_CODE,
+ t2.ST_CLASS_NAME,
+ t3.ST_STUDENT_ID,
+ t5.ST_TEST_CATEGORY_CODE,
+ t5.ST_TEST_CATEGORY,
+ t5.NM_VALUE,
+ t5.ST_GRADE
+FROM
+ DM_SCHOOL t1,
+ DM_CLASS t2,
+ DM_STUDENT t3,
+ DM_STUDENT_EXTEND t4,
+ DM_TEST_CATEGORY_RECORD t5
+WHERE
+ t1.ST_SCHOOL_ID = t2.ST_SCHOOL_ID
+AND t2.ST_CLASS_ID = t4.ST_CLASS_ID
+AND t3.ST_STUDENT_ID = t4.ST_STUDENT_ID
+AND t4.ST_STUDENT_EXTEND_ID = t5.ST_STUDENT_EXTEND_ID
+AND t3.ST_SEX = '男'
+AND t2.ST_CLASS_GRADE_CODE = '31'
+AND t4.ST_YEAR = '2017'
+AND t1.ST_NAME = '000080学校'
+ORDER BY t3.ST_STUDENT_ID
+
+
+
+--行转列
+SELECT
+ t3.ST_STUDENT_ID,
+ t3.ST_NAME,
+ sum(case t5.ST_TEST_CATEGORY when '肺活量' then t5.NM_VALUE else 0 end) '肺活量',
+ sum(case t5.ST_TEST_CATEGORY when '体重指数(BMI)' then t5.NM_VALUE else 0 end) '体重指数(BMI)',
+ sum(case t5.ST_TEST_CATEGORY when '50米跑' then t5.NM_VALUE else 0 end) '50米跑',
+ sum(case t5.ST_TEST_CATEGORY when '1000米跑' then t5.NM_VALUE else 0 end) '1000米跑',
+ sum(case t5.ST_TEST_CATEGORY when '立定跳远' then t5.NM_VALUE else 0 end) '立定跳远',
+ sum(case t5.ST_TEST_CATEGORY when '引体向上' then t5.NM_VALUE else 0 end) '引体向上',
+ sum(case t5.ST_TEST_CATEGORY when '坐位体前屈' then t5.NM_VALUE else 0 end) '坐位体前屈',
+ sum(case t5.ST_TEST_CATEGORY when '总分' then t5.NM_VALUE else 0 end) '总分'
+FROM
+ DM_SCHOOL t1,
+ DM_CLASS t2,
+ DM_STUDENT t3,
+ DM_STUDENT_EXTEND t4,
+ DM_TEST_CATEGORY_RECORD t5
+WHERE
+ t1.ST_SCHOOL_ID = t2.ST_SCHOOL_ID
+AND t2.ST_CLASS_ID = t4.ST_CLASS_ID
+AND t3.ST_STUDENT_ID = t4.ST_STUDENT_ID
+AND t4.ST_STUDENT_EXTEND_ID = t5.ST_STUDENT_EXTEND_ID
+AND t3.ST_SEX = '男'
+AND t2.ST_CLASS_GRADE_CODE = '31'
+AND t4.ST_YEAR = '2017'
+AND t5.ST_TEST_CATEGORY_CODE in('BMI', '04', '05', '06', '07', '11', '14', 'sum')
+AND t1.ST_NAME = '000080学校'
+GROUP BY t3.ST_STUDENT_ID
